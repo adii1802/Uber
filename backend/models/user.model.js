c8d1f618964426d452305ea2d33ac59369593761
Aditya Gaur <adityagaur1802@gmail.com>
2025-11-10 13:44:32 +0530

    user authentication done

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -32,7 +32,7 @@ const userSchema = new mongoose.Schema({
     
 })
 userSchema.methods.generateAuthToken = function() {
-    const token = jwt.sign({ _id: this._id, email: this.email }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    const token = jwt.sign({ _id: this._id, email: this.email }, process.env.JWT_SECRET, { expiresIn: '24h' });
     return token;
 }
 userSchema.methods.comparePassword = async function(candidatePassword) {
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -2,7 +2,8 @@ const express=require('express');
 const router=express.Router();
 const {body}=require('express-validator');
 const userController=require('../controllers/user.controller'); 
-
+const authUser = require('../middlewares/auth.middleware');
+    
 router.post('/register',[
     body('email').isEmail().withMessage('Invalid email address'),
     body('fullname.firstname ').isLength({min:3}).withMessage('Firstname must be at least 3 characters long'),
@@ -14,5 +15,10 @@ router.post('/login', [
     body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
 ], userController.loginUser
 )
+
+router.get('/profile',authMiddleware.authUser, userController.getUserProfile);
+router.get('/logout',authMiddleware.authUser, userController.logoutUser);
+
+
 
 module.exports=router;
